0b19af717bcf3f778f23cdc85f35ab6e89dbcbec
Anita Samchuk <anita232110@gmail.com>
2020-10-13 20:53:58 +0300

    Реализованы функции encrypt_caesar() и decrypt_caesar()

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -1,6 +1,3 @@
-import typing as tp
-
-
 def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     """
     Encrypts plaintext using a Caesar cipher.
@@ -15,7 +12,19 @@ def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     ''
     """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    for i in plaintext:
+        if ord('a') <= ord(i) <= ord('z'):
+            if ord(i) + shift > ord('z'):
+                ciphertext += chr(ord(i) + shift - 26)
+            else:
+                ciphertext += chr(ord(i) + shift)
+        elif ord('A') <= ord(i) <= ord('Z'):
+            if ord(i) + shift > ord('Z'):
+                ciphertext += chr(ord(i) + shift - 26)
+            else:
+                ciphertext += chr(ord(i) + shift)
+        else:
+            ciphertext += i
     return ciphertext
 
 
@@ -33,14 +42,17 @@ def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
     ''
     """
     plaintext = ""
-    # PUT YOUR CODE HERE
+    for i in ciphertext:
+        if ord('a') <= ord(i) <= ord('z'):
+            if ord(i) - shift < ord('a'):
+                plaintext += chr(ord(i) - shift + 26)
+            else:
+                plaintext += chr(ord(i) - shift)
+        elif ord('A') <= ord(i) <= ord('Z'):
+            if ord(i) - shift < ord('A'):
+                plaintext += chr(ord(i) - shift + 26)
+            else:
+                plaintext += chr(ord(i) - shift)
+        else:
+            plaintext += i
     return plaintext
-
-
-def caesar_breaker_brute_force(ciphertext: str, dictionary: tp.Set[str]) -> int:
-    """
-    Brute force breaking a Caesar cipher.
-    """
-    best_shift = 0
-    # PUT YOUR CODE HERE
-    return best_shift

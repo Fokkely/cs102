8630c1501aa376ca6e6a6bd9fc7d16b55934cc95
Anita Samchuk <anita232110@gmail.com>
2020-10-13 21:16:51 +0300

    Реализованы функции encrypt_vigenere() и decrypt_vigenere()

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -10,7 +10,22 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     'LXFOPVEFRNHR'
     """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    k = 0
+    for i in plaintext:
+        shift = ord(keyword[k % len(keyword)].lower())-97
+        k += 1
+        if ord('a') <= ord(i) <= ord('z'):
+            if ord(i) + shift > ord('z'):
+                ciphertext += chr(ord(i) + shift - 26)
+            else:
+                ciphertext += chr(ord(i) + shift)
+        elif ord('A') <= ord(i) <= ord('Z'):
+            if ord(i) + shift > ord('Z'):
+                ciphertext += chr(ord(i) + shift - 26)
+            else:
+                ciphertext += chr(ord(i) + shift)
+        else:
+            ciphertext += i
     return ciphertext
 
 
@@ -26,5 +41,20 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     'ATTACKATDAWN'
     """
     plaintext = ""
-    # PUT YOUR CODE HERE
+    k = 0
+    for i in ciphertext:
+        shift = ord(keyword[k % len(keyword)].lower())-97
+        k += 1
+        if ord('a') <= ord(i) <= ord('z'):
+            if ord(i) - shift < ord('a'):
+                plaintext += chr(ord(i) - shift + 26)
+            else:
+                plaintext += chr(ord(i) - shift)
+        elif ord('A') <= ord(i) <= ord('Z'):
+            if ord(i) - shift < ord('A'):
+                plaintext += chr(ord(i) - shift + 26)
+            else:
+                plaintext += chr(ord(i) - shift)
+        else:
+            plaintext += i
     return plaintext

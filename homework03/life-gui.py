eb2bbedda9423f4b46411723f7c7a2633c51839e
Anita Samchuk <71382097+Fokkely@users.noreply.github.com>
2021-01-19 03:57:46 +0300

    git add life-gui.py

--- homework03/life-gui.py
+++ homework03/life-gui.py
@@ -5,9 +5,6 @@ from ui import UI
 
 
 class GUI(UI):
-    """
-    хеллоу
-    """
     def __init__(self, life: GameOfLife, cell_size: int = 10, speed: int = 10) -> None:
         super().__init__(life)
         self.cell_size = cell_size
@@ -27,9 +24,13 @@ class GUI(UI):
     def draw_lines(self) -> None:
         # Copy from previous assignment
         for x in range(0, self.width, self.cell_size):
-            pygame.draw.line(self.screen, pygame.Color("black"), (x, 0), (x, self.height))
+            pygame.draw.line(
+                self.screen, pygame.Color("black"), (x, 0), (x, self.height)
+            )
         for y in range(0, self.height, self.cell_size):
-            pygame.draw.line(self.screen, pygame.Color("black"), (0, y), (self.width, y))
+            pygame.draw.line(
+                self.screen, pygame.Color("black"), (0, y), (self.width, y)
+            )
 
     def draw_grid(self) -> None:
         # Copy from previous assignment

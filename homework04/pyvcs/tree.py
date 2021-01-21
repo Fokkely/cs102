ded0c7d060ed0efd9f3ff1eff018b22106203959
Anita Samchuk <71382097+Fokkely@users.noreply.github.com>
2021-01-21 21:10:43 +0300

    fixing mypy

--- homework04/pyvcs/tree.py
+++ homework04/pyvcs/tree.py
@@ -56,8 +56,10 @@ def commit_tree(
 ) -> str:
     if author is None and "GIT_AUTHOR_NAME" in os.environ and "GIT_AUTHOR_EMAIL" in os.environ:
         author = (
-            os.getenv("GIT_AUTHOR_NAME", None) + " " +  f'<{os.getenv("GIT_AUTHOR_EMAIL", None)}>' # type:ignore
-        )  # type:ignore
+            os.getenv("GIT_AUTHOR_NAME", None)
+            + " "  # type:ignore
+            + f'<{os.getenv("GIT_AUTHOR_EMAIL", None)}>'  # type:ignore
+        )
     if time.timezone > 0:
         tz_str = "-"
     else:

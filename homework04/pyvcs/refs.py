1d506a7ee2abf4a7918089290988e79f293aba15
Anita Samchuk <71382097+Fokkely@users.noreply.github.com>
2021-01-21 19:17:52 +0300

    first try

--- homework04/pyvcs/refs.py
+++ homework04/pyvcs/refs.py
@@ -3,30 +3,37 @@ import typing as tp
 
 
 def update_ref(gitdir: pathlib.Path, ref: tp.Union[str, pathlib.Path], new_value: str) -> None:
-    # PUT YOUR CODE HERE
-    ...
+    with (gitdir / ref).open("w") as f:
+        f.write(new_value)
 
 
 def symbolic_ref(gitdir: pathlib.Path, name: str, ref: str) -> None:
-    # PUT YOUR CODE HERE
     ...
 
 
 def ref_resolve(gitdir: pathlib.Path, refname: str) -> str:
-    # PUT YOUR CODE HERE
-    ...
+    if refname == "HEAD" and not is_detached(gitdir):
+        return ref_resolve(gitdir, get_ref(gitdir))
+    with (gitdir / refname).open() as f:
+        return f.read().strip()
 
 
 def resolve_head(gitdir: pathlib.Path) -> tp.Optional[str]:
-    # PUT YOUR CODE HERE
-    ...
+    ref = get_ref(gitdir)
+    if (gitdir / ref).exists():
+        return ref_resolve(gitdir, ref)
+    else:
+        return None
 
 
 def is_detached(gitdir: pathlib.Path) -> bool:
-    # PUT YOUR CODE HERE
-    ...
+    try:
+        get_ref(gitdir)
+        return False
+    except IndexError:
+        return True
 
 
 def get_ref(gitdir: pathlib.Path) -> str:
-    # PUT YOUR CODE HERE
-    ...
+    with (gitdir / "HEAD").open() as file:
+        return file.read().strip().split()[1]

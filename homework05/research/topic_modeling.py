64c51470c4bda2f932a64678e76cd70ba1b9254a
Anita Samchuk <71382097+Fokkely@users.noreply.github.com>
2021-01-26 02:40:19 +0300

    black

--- homework05/research/topic_modeling.py
+++ homework05/research/topic_modeling.py
@@ -11,12 +11,12 @@ def example():
     posts = get_wall_execute(domain="rbc", count=5000, max_count=1000, progress=tqdm)
     stopwords = list(map(str.strip, open("stop_words.txt")))
     text_no_urls = map(
-    preprocessing.replace.replace_urls, posts.text.dropna().to_list()
+        preprocessing.replace.replace_urls, posts.text.dropna().to_list()
     )
     text_no_punct = map(preprocessing.remove_punctuation, text_no_urls)
     text_no_emojis = map(preprocessing.replace.replace_emojis, text_no_punct)
     text_no_white_space = map(
-    preprocessing.normalize.normalize_whitespace, text_no_emojis
+        preprocessing.normalize.normalize_whitespace, text_no_emojis
     )
     docs = map(str.split, text_no_white_space)
     docs = [[word.lower() for word in doc if word not in stopwords] for doc in docs]
@@ -24,7 +24,7 @@ def example():
     dictionary = Dictionary(docs)
     corpus = list(dictionary.doc2bow(text) for text in docs)
     ldamodel = gensim.models.ldamodel.LdaModel(
-    corpus, num_topics=10, id2word=dictionary, passes=15
+        corpus, num_topics=10, id2word=dictionary, passes=15
     )
     vis = pyLDAvis.gensim.prepare(ldamodel, corpus, dictionary)
     pyLDAvis.show(vis)

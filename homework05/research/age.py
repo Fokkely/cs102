2dd7398d64cb9ca3989a22573488d2100316d616
Anita Samchuk <71382097+Fokkely@users.noreply.github.com>
2021-01-26 03:52:08 +0300

    age

--- homework05/research/age.py
+++ homework05/research/age.py
@@ -15,7 +15,7 @@ def age_predict(user_id: int) -> tp.Optional[float]:
     """
     ages = []
     friends = get_friends(user_id, fields=["bdate"])
-    for friend in friends:
+    for friend in friends.items:
         try:
             birthdate = dt.datetime.strptime(friend["bdate"], "%d.%m.%Y")  # type: ignore
             age = relativedelta(dt.datetime.now(), birthdate).years

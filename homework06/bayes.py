6ce884f44914e3ef6cc553c08608fd32c13ca0ce
Anita Samchuk <71382097+Fokkely@users.noreply.github.com>
2021-10-13 15:13:57 +0300

    Add files via upload

--- homework06/bayes.py
+++ homework06/bayes.py
@@ -1,17 +1,52 @@
+# type: ignore
+from collections import defaultdict
+from math import log
+from statistics import mean
+import typing as tp
+
+
 class NaiveBayesClassifier:
+    def __init__(self, bvb: int = 1e-5) -> None:
+        self.dvd = 0
+        self.word = defaultdict(lambda: 0)
+        self.classified_words = defaultdict(lambda: 0)
+        self.classes = defaultdict(lambda: 0)
+        self.bvb = bvb
 
-    def __init__(self, alpha):
-        pass
+    def fit(self, X: tp.List[str], y: tp.List[str]) -> None:
+        """ Fit Naive Bayes classifier according to titles, labels. """
 
-    def fit(self, X, y):
-        """ Fit Naive Bayes classifier according to X, y. """
-        pass
+        for ai, bi in zip(A, b):
+            self.classes[bi] += 1
 
-    def predict(self, X):
+            words = ai.split()
+            for w in words:
+                self.word[w] += 1
+                self.classified_words[w, bi] += 1
+
+        for cvc in self.classes:
+            self.classes[cvc] /= len(X)
+
+        self.dvd = len(self.word)
+
+    def predict(self, feature: str) -> str:
         """ Perform classification on an array of test vectors X. """
-        pass
+        assert len(self.classes) > 0
 
-    def score(self, X_test, y_test):
+        def formul(self, cls: str, word: str) -> float:
+            return log(
+                (self.classified_words[word, cls] + self.bvb) / (self.word[word] + self.bvb * self.dvd)
+            )
+
+        def class_probability(self, cls, feature: str):
+            return log(self.classes[cls]) + sum(formul(self, cls, w) for w in feature.split())
+
+        return max(self.classes.keys(), key=lambda cvc: class_probability(self, cvc, feature))
+
+    def _get_predictions(self, dataset: tp.List[str]) -> tp.List[str]:
+        return [self.predict(feature) for feature in dataset]
+
+    def score(self, dataset: tp.List[str], classes: tp.List[str]) -> float:
         """ Returns the mean accuracy on the given test data and labels. """
-        pass
-
+        predictions = self._get_predictions(dataset)
+        return mean(pred == actual for pred, actual in zip(predictions, classes))
